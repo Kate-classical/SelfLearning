658babf6e1076148d27ed30aef12a50d090e5ccb
“Aliaksei <ITalex23@yandex.ru>
2021-09-26 16:17:35 +0300

    berestovitca

--- src/by/alexrom/s01/Task01.java
+++ src/by/alexrom/s01/Task01.java
@@ -3,6 +3,6 @@ package by.alexrom.s01;
 public class Task01 {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello berestovitca");
     }
 }
