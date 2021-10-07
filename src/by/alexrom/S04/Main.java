1c45a1a422e575bb5cf7fa7c8ee55d73131a1bc5
“Aliaksei <ITalex23@yandex.ru>
2021-10-07 21:07:58 +0300

    S04

---
+++ src/by/alexrom/S04/Main.java
@@ -0,0 +1,14 @@
+package by.alexrom.S04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        alfabetPosition();
+    }
+
+    static void alfabetPosition() {
+        TaskEnum taskEnum = TaskEnum.F;
+        System.out.println("Letter number is: " + taskEnum.alfabetPosition());
+    }
+
+}
